cda6446ae1faa7df3272135af3e177c39436b19f
Celes <celestinomorenorodriguez@gmail.com>
2024-03-09 21:45:40 -0800

    subiendo cambios

--- js/voice-search.js
+++ js/voice-search.js
@@ -40,3 +40,5 @@ const voiceRecognition = () =>{
 document.querySelector('.form__microphone-icon').addEventListener("click",voiceSearchModalOpen);
 document.querySelector(".voice-search__close-modal").addEventListener("click",voiceSearchModalClose);
 document.querySelector(".voice-search__microphone-border").addEventListener("click",voiceRecognition)
+
+console.log("hola mundooooo");
